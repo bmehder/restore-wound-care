682fbc04aa515bccf5e100397b7bc3f30eedf6df
bmehder <bmehder@gmail.com>
2023-06-21 20:25:51 +0100

    add new team pic

--- src/routes/about/data.js
+++ src/routes/about/data.js
@@ -1,17 +1,17 @@
 export default [
   {
-    src: 'https://restorewoundcare.com/wp-content/uploads/lilliane-sarraff-md.jpg',
+    src: '/team/lilliane-sarraff-md.jpg',
     name: 'Lilliane Sarraff, M.D.',
     description: `Dr. Lilliane Sarraff is a native of Florida and received her MD at Ross University. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and giving patients back their ability to participate in the things they love. Dr. Sarraff joined RWC in 2022, and when she’s not helping patients, she can be found in her garden.`,
   },
   {
-    src: 'https://restorewoundcare.com/wp-content/uploads/melanie-markadakis.jpg',
+    src: '/team/melanie-markadakis.jpg',
     // name: 'Melanie Markadakis, MSN, APRN, FNP-C',
     name: 'M. Markadakis, MSN, APRN, FNP-C',
     description: `Melanie Markadakis is a native of Earth and received her degree from the School of Hard Knocks – she took night classes. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and giving patients back their ability to participate in the things they love. Melanie joined RWC in 2023, and when she’s not helping patients, she's not helping patients, innit.`,
   },
   {
-    src: 'https://restorewoundcare.com/wp-content/uploads/kate-thompson.jpg',
+    src: '/team/kate-thompson.jpg',
     name: 'Kate Thompson, FNP-C',
     description: `Kate Thompson is a native of Maryland and received her BSN, MSN and FNP-C at The
       University of Northern Colorado. She proudly serves patients in northern Colorado
@@ -21,12 +21,12 @@ export default [
       with her family.`,
   },
   {
-    src: 'https://restorewoundcare.com/wp-content/uploads/elizabeth-cirbo.jpg',
+    src: '/team/elizabeth-cirbo.jpg',
     name: 'Elizabeth Cirbo, FNP-BC, DNP',
     description: `Elizabeth Cirbo is a native of Earth and received her degree from the School of Hard Knocks – she took night classes. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and giving patients back their ability to participate in the things they love. Elizabeth joined RWC in 2023, and when she’s not helping patients, she's not helping patients, innit.`,
   },
   {
-    src: 'https://restorewoundcare.com/wp-content/uploads/shaun-baillie.jpg',
+    src: '/team/shaun-baillie.jpg',
     name: 'Shaun Baillie, APN',
     description: `Shaun Baillie is a native of Pennsylvania and received his BSN and DNP at The
       University of Pittsburgh. He proudly serves patients in Jefferson county and
