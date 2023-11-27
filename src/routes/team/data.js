aeccfed2246cb2c05c99e2ad972baac55b9a1c97
bmehder <bmehder@gmail.com>
2023-11-27 21:24:08 +0000

    added Rose and another

--- src/routes/team/data.js
+++ src/routes/team/data.js
@@ -1,52 +1,57 @@
 export default [
-  {
-    src: '/team/lilliane-sarraff.jpg',
-    name: 'Lilliane Sarraff, M.D.',
-    description: `Dr. Lilliane Sarraff is a native of Florida and received her MD at Ross University. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and giving patients back their ability to participate in the things they love. Dr. Sarraff joined RWC in 2022, and when she’s not helping patients, she can be found in her garden.`,
-  },
-  {
-    src: '/team/kate-thompson.jpg',
-    name: 'Kate Thompson, FNP-C',
-    description: `Kate Thompson is a native of Maryland and received her BSN, MSN and FNP-C at The
+	{
+		src: '/team/lilliane-sarraff.jpg',
+		name: 'Lilliane Sarraff, M.D.',
+		description: `Dr. Lilliane Sarraff is a native of Florida and received her MD at Ross University. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and giving patients back their ability to participate in the things they love. Dr. Sarraff joined RWC in 2022, and when she’s not helping patients, she can be found in her garden.`,
+	},
+	{
+		src: '/team/kate-thompson.jpg',
+		name: 'Kate Thompson, FNP-C',
+		description: `Kate Thompson is a native of Maryland and received her BSN, MSN and FNP-C at The
       University of Northern Colorado. She proudly serves patients in northern Denver
       and surrounding areas. She is passionate about excellent wound care and providing an
       at-home option for those who have difficulty traveling. Kate joined RWC in early
       2023, and when she’s not helping patients, she can be found exploring the outdoors
       with her family.`,
-  },
-  {
-    src: '/team/elizabeth-cirbo.jpg',
-    name: 'Elizabeth Cirbo, FNP-BC, DNP',
-    description: `Elizabeth Cirbo is a native of Wyoming and received her DNP, FNP-BC from the University of Northern Colorado and BSN from the University of Wyoming. She proudly serves patients in northern Denver and surrounding areas. She is passionate about excellent wound care and getting to know patients in the comfort of their own home. Liz joined RWC in March 2023 and is passionate about excellent patient care and getting to know patients in the comfort of their own home. In her down time, she enjoys trying new recipes for her family, reading a good book and hiking.`,
-  },
-  {
-    src: '/team/patricia-caporale.jpg',
-    name: 'Patricia Caporale, FNP-C',
-    description: `Trish Caporale is a native of New Jersey and received her MSN from United States University in San Diego. She is passionate about excellent wound care and advocating for patients who are unable to care for their wounds. Trish joined RWC in 2023, and when she’s not helping patients, you can find her cooking or hiking with her corgi, Gus.`,
-  },
-  // {
-  //   src: '/team/sally-turnipseed.jpg',
-  //   name: 'Sally Turnipseed, FNP-C',
-  //   description: `Sally Turnipseed is a native of Tennessee and received her BSN at Belmont University and MSN at Regis University, Denver. She is passionate about excellent care and witnessing chronic wounds get better at home. Sally joined RWC in 2023 and when she’s not helping patients you can find her gardening, spending time with family, including her bulldog, Frank.`,
-  // },
-  {
-    src: '/team/sharise-reynolds.jpg',
-    name: 'Sharise Reynolds',
-    description: `Sharise Reynolds is a native of Colorado and has a background in healthcare. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and loves helping people and seeing results in their wounds and overall health. Sharise joined RWC in 2021, and when she’s not helping patients, you can find her swimming, hiking and enjoying the outdoors.`,
-  },
-  {
-    src: '/team/cecilia-martinez.jpg',
-    name: 'Cecilia Martinez',
-    description: `Cecilia Martinez is a native of Texas but was raised in Colorado. She proudly serves patients in northern Denver and surrounding areas. She is passionate about excellent wound care and loves seeing patients smiling faces when a wound is finally healed. Cecilia joined RWC in 2021, and when she’s not helping patients, you can find her enjoying time with her family and grandchildren.`,
-  },
-  // {
-  //   src: '/team/heather-campos.jpg',
-  //   name: 'Heather Campos',
-  //   description: `Heather Campos is a native of Texas and has a background in healthcare. She proudly serves patients on the phone and enjoys ensuring ease and care when they contact our office. Heather joined RWC in 2023, and when she's not helping patients, she can be found reading or at game night with her family and friends.`,
-  // },
-  {
-    src: '/team/denise-cummins.jpg',
-    name: 'Denise Cummins',
-    description: `Denise Cummins is a native of Wisconsin and was raised in Nevada. She proudly serves patients on the phone and through the billing process. She is passionate about excellent wound care and enjoys witnessing the results of the treatments we provide. Denise joined RWC in 2022, and when she’s not assisting patients, you can find her traveling and spending time with her son.`,
-  },
+	},
+	{
+		src: '/team/elizabeth-cirbo.jpg',
+		name: 'Elizabeth Cirbo, FNP-BC, DNP',
+		description: `Elizabeth Cirbo is a native of Wyoming and received her DNP, FNP-BC from the University of Northern Colorado and BSN from the University of Wyoming. She proudly serves patients in northern Denver and surrounding areas. She is passionate about excellent wound care and getting to know patients in the comfort of their own home. Liz joined RWC in March 2023 and is passionate about excellent patient care and getting to know patients in the comfort of their own home. In her down time, she enjoys trying new recipes for her family, reading a good book and hiking.`,
+	},
+	{
+		src: '/team/patricia-caporale.jpg',
+		name: 'Patricia Caporale, FNP-C',
+		description: `Trish Caporale is a native of New Jersey and received her MSN from United States University in San Diego. She is passionate about excellent wound care and advocating for patients who are unable to care for their wounds. Trish joined RWC in 2023, and when she’s not helping patients, you can find her cooking or hiking with her corgi, Gus.`,
+	},
+	{
+		src: '/team/lindsey-e-rehl.jpg',
+		name: 'Lindsey E. Rehl, DNP, ANP, FNP-BC',
+		description: `Lindsey Rehl is a native of Pennsylvania and received her DNP at Temple University, her BSN at Drexel University and her BS at Valdosta State University. She is passionate about excellent wound care and healing so patients can get back to participating in daily activities and the things that bring them joy. Lindsey joined RWC in 2023 and when she’s not treating patients you can find her ballroom dancing, enjoying the outdoors and spending time with her family.`,
+	},
+	{
+		src: '/team/rose-wachira.jpg',
+		name: 'Rose Wachira, APRN',
+		description: ``,
+	},
+	{
+		src: '/team/sharise-reynolds.jpg',
+		name: 'Sharise Reynolds',
+		description: `Sharise Reynolds is a native of Colorado and has a background in healthcare. She proudly serves patients in southern Denver and surrounding areas. She is passionate about excellent wound care and loves helping people and seeing results in their wounds and overall health. Sharise joined RWC in 2021, and when she’s not helping patients, you can find her swimming, hiking and enjoying the outdoors.`,
+	},
+	{
+		src: '/team/cecilia-martinez.jpg',
+		name: 'Cecilia Martinez',
+		description: `Cecilia Martinez is a native of Texas but was raised in Colorado. She proudly serves patients in northern Denver and surrounding areas. She is passionate about excellent wound care and loves seeing patients smiling faces when a wound is finally healed. Cecilia joined RWC in 2021, and when she’s not helping patients, you can find her enjoying time with her family and grandchildren.`,
+	},
+	// {
+	//   src: '/team/heather-campos.jpg',
+	//   name: 'Heather Campos',
+	//   description: `Heather Campos is a native of Texas and has a background in healthcare. She proudly serves patients on the phone and enjoys ensuring ease and care when they contact our office. Heather joined RWC in 2023, and when she's not helping patients, she can be found reading or at game night with her family and friends.`,
+	// },
+	{
+		src: '/team/denise-cummins.jpg',
+		name: 'Denise Cummins',
+		description: `Denise Cummins is a native of Wisconsin and was raised in Nevada. She proudly serves patients on the phone and through the billing process. She is passionate about excellent wound care and enjoys witnessing the results of the treatments we provide. Denise joined RWC in 2022, and when she’s not assisting patients, you can find her traveling and spending time with her son.`,
+	},
 ]
